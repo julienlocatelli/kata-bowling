b7b5437f4ada5a0a082d2cef0bdfb820681cf8b1
Julien Locatelli <jlocatelli@OrdinatdeJulien.lan>
2023-03-01 12:09:10 +0100

    test(): unit test bowling game

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,37 @@
 class Game {
   constructor() {
-    this._score = 0;
+    this._roll = [];
   }
 
   /**
    * @param {number} pins
    * @return {void}
    */
-  roll(pins) {}
+  roll(pins) {
+    this._roll.push(pins);
+  }
 
   /**
    * @returns {number} the final score of one game
    */
   get score() {
-    return this._score;
+    let score = 0;
+
+    let i = 0;
+    for (let turn = 0; turn < 10; turn++) {
+      if (this._roll[i] === 10) {
+        score += 10 + this._roll[i + 1] + this._roll[i + 2];
+        i += 1;
+      } else {
+        if (this._roll[i] + this._roll[i + 1] === 10) {
+          score += 10 + this._roll[i + 2];
+        } else {
+          score += this._roll[i] + this._roll[i + 1];
+        }
+        i += 2;
+      }
+    }
+    return score;
   }
 }
 
--- src/bowling.spec.js
+++ src/bowling.spec.js
@@ -1,13 +1,49 @@
 import Game from "./Game";
 
+let game;
+const rollMany = (nbRoll, score, game) => {
+  for (let i = 0; i < nbRoll; i++) {
+    game.roll(score);
+  }
+};
 describe("#Bowling game", () => {
-  it("should score 0 for gutter game");
+  beforeEach(() => {
+    game = new Game();
+  });
 
-  it("should score 20 for all ones game");
+  it("should score 0 for gutter game", () => {
+    rollMany(20, 0, game);
 
-  it("should score 16 for a spare followed by a 3 ball");
+    expect(game.score).toEqual(0);
+  });
 
-  it("should score 24 for a strike followed by a 3 and a 4 ball");
+  it("should score 20 for all ones game", () => {
+    rollMany(20, 1, game);
 
-  it("should score 300 for perfect game");
+    expect(game.score).toEqual(20);
+  });
+
+  it("should score 16 for a spare followed by a 3 ball", () => {
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(17, 0, game);
+
+    expect(game.score).toEqual(16);
+  });
+
+  it("should score 24 for a strike followed by a 3 and a 4 ball", () => {
+    game.roll(10);
+    game.roll(3);
+    game.roll(4);
+    rollMany(16, 0, game);
+
+    expect(game.score).toEqual(24);
+  });
+
+  it("should score 300 for perfect game", () => {
+    rollMany(12, 10, game);
+
+    expect(game.score).toEqual(300);
+  });
 });
